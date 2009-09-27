d1e0c32c65dc13255763f6e30ba9a0f8f7ddc33f
Leonid Movshovich <event.riga@gmail.com>
2009-09-27 13:45:04 +0300

    Add create repo command implementation

---
+++ CreateSpimRepo.hs
@@ -0,0 +1,20 @@
+-- add a vcard/vcalendar file into the pim graph
+module Main where
+
+import IO
+import qualified System.Environment as SysEnv
+import qualified System.Directory as SysDir
+import qualified System.Cmd as Cmd
+import qualified System.Exit as Exit
+
+main :: IO()
+main = do 
+  args <- SysEnv.getArgs
+  SysDir.createDirectoryIfMissing True (head args)
+  SysDir.setCurrentDirectory (head args)
+  exitCode <- Cmd.system "git init"  
+  case exitCode of 
+    Exit.ExitSuccess -> do putStr "Successfully created new personal data repository\n"
+    Exit.ExitFailure x -> do putStr ("Repository creation failed: git exit code" 
+                                     ++ show x ++ "\n")
+
