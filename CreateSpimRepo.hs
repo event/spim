10325f2860ccebcfcc2812be582e7760a2b7c304
Leonid Movshovich <event.riga@gmail.com>
2009-11-08 22:08:25 +0200

    + create index folder when creating repo

--- CreateSpimRepo.hs
+++ CreateSpimRepo.hs
@@ -1,4 +1,4 @@
-module Main where
+hmodule Main where
 
 import IO
 import qualified System.Environment as SysEnv
@@ -13,7 +13,9 @@ main = do
   SysDir.setCurrentDirectory (head args)
   exitCode <- Cmd.system "git init"  
   case exitCode of 
-    Exit.ExitSuccess -> do putStr "Successfully created new personal data repository\n"
-    Exit.ExitFailure x -> do putStr ("Repository creation failed: git exit code" 
+    Exit.ExitSuccess -> do SysDir.createDirectory "indices"
+                           putStr "Successfully created new personal data repository\n"
+                           
+    Exit.ExitFailure x -> do putStr ("Repository creation failed: git exit code: " 
                                      ++ show x ++ "\n")
 
