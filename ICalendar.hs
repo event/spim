b04e6533e5fbe149e405f49617d27a885f757f5a
Leonid Movshovich <event.riga@gmail.com>
2009-12-13 15:19:05 +0200

    - vcard copy-paste + icalendar correct stuff

--- ICalendar.hs
+++ ICalendar.hs
@@ -8,12 +8,12 @@ import Data.List as List
 
 type ICal = MD.MIMEDir
 
-toVCard :: String -> Maybe ICal
-toVCard s = let vc = MD.mimeDirFromString s in
-            if checkICal vc then
-                Just vc
-            else
-                Nothing
+toICal :: String -> Maybe ICal
+toICal s = let vc = MD.mimeDirFromString s in
+           if checkICal vc then
+               Just vc
+           else
+               Nothing
 
 checkICal :: ICal -> Bool
 checkICal ic = MD.kind ic == "VCALENDAR" && MD.isMIMEDirValid icalCLCheck ic
@@ -33,35 +33,8 @@ anyAllowed :: a -> Bool
 anyAllowed x = True
 
 stdProps :: Map.Map MD.PropName (MD.PropValue -> Bool, MD.Parameters -> Bool)
-stdProps = Map.fromList [("NAME", (anyAllowed, notAllowed))
-                        , ("PROFILE", ((=="VCARD"), notAllowed))
-                        , ("SOURCE", (anyAllowed, anyAllowed))
-                        , ("FN", (anyAllowed, anyAllowed))
-                        , ("N" ,(anyAllowed, anyAllowed))
-                        , ("NICKNAME", (anyAllowed, anyAllowed))
-                        , ("PHOTO" ,(anyAllowed, anyAllowed))
-                        , ("BDAY", (anyAllowed, anyAllowed))
-                        , ("ADR" ,(anyAllowed, anyAllowed))
-                        , ("LABEL", (anyAllowed, anyAllowed))
-                        , ("TEL" ,(anyAllowed, anyAllowed))
-                        , ("EMAIL", (anyAllowed, anyAllowed))
-                        , ("MAILER" ,(anyAllowed, anyAllowed))
-                        , ("TZ", (anyAllowed, anyAllowed))
-                        , ("GEO" ,(anyAllowed, anyAllowed))
-                        , ("TITLE", (anyAllowed, anyAllowed))
-                        , ("ROLE" ,(anyAllowed, anyAllowed))
-                        , ("LOGO", (anyAllowed, anyAllowed))
-                        , ("AGENT" ,(anyAllowed, anyAllowed))
-                        , ("ORG", (anyAllowed, anyAllowed))
-                        , ("CATEGORIES" ,(anyAllowed, anyAllowed))
-                        , ("NOTE", (anyAllowed, anyAllowed))
-                        , ("PRODID" ,(anyAllowed, anyAllowed))
-                        , ("REV", (anyAllowed, anyAllowed))
-                        , ("SORT-STRING" ,(anyAllowed, anyAllowed))
-                        , ("SOUND", (anyAllowed, anyAllowed))
-                        , ("UID" ,(anyAllowed, anyAllowed))
-                        , ("URL", (anyAllowed, anyAllowed))
-                        , ("VERSION" ,((=="3.0"), anyAllowed))
-                        , ("CLASS" ,(anyAllowed, anyAllowed))
-                        , ("KEY", (anyAllowed, anyAllowed))
+stdProps = Map.fromList [("PRODID", (anyAllowed, notAllowed))
+                        , ("VERSION", (anyAllowed, notAllowed))
+                        , ("CALCSCALE", (anyAllowed, anyAllowed))
+                        , ("METHOD", (anyAllowed, anyAllowed))
                         ]
